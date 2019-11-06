e34faac4d6d6609feebe867b3269e39a40bd0610
Telmo-Alves <tja@cavalodetroia.club>
2019-11-06 19:33:41 +0000

    nicial

---
+++ Ficheiros/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbN_Botoes = New System.Windows.Forms.TextBox()
+        Me.tbLogo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.bLerConfig = New System.Windows.Forms.Button()
+        Me.bGravarConfig = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(86, 95)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "N_Botoes"
+        '
+        'tbN_Botoes
+        '
+        Me.tbN_Botoes.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbN_Botoes.Location = New System.Drawing.Point(262, 91)
+        Me.tbN_Botoes.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.tbN_Botoes.Name = "tbN_Botoes"
+        Me.tbN_Botoes.Size = New System.Drawing.Size(148, 26)
+        Me.tbN_Botoes.TabIndex = 1
+        '
+        'tbLogo
+        '
+        Me.tbLogo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbLogo.Location = New System.Drawing.Point(262, 149)
+        Me.tbLogo.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.tbLogo.Name = "tbLogo"
+        Me.tbLogo.Size = New System.Drawing.Size(148, 26)
+        Me.tbLogo.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(86, 154)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Logo"
+        '
+        'bLerConfig
+        '
+        Me.bLerConfig.Location = New System.Drawing.Point(76, 244)
+        Me.bLerConfig.Name = "bLerConfig"
+        Me.bLerConfig.Size = New System.Drawing.Size(142, 28)
+        Me.bLerConfig.TabIndex = 4
+        Me.bLerConfig.Text = "Ler Config"
+        Me.bLerConfig.UseVisualStyleBackColor = True
+        '
+        'bGravarConfig
+        '
+        Me.bGravarConfig.Location = New System.Drawing.Point(303, 244)
+        Me.bGravarConfig.Name = "bGravarConfig"
+        Me.bGravarConfig.Size = New System.Drawing.Size(142, 28)
+        Me.bGravarConfig.TabIndex = 5
+        Me.bGravarConfig.Text = "Gravar Config"
+        Me.bGravarConfig.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(758, 439)
+        Me.Controls.Add(Me.bGravarConfig)
+        Me.Controls.Add(Me.bLerConfig)
+        Me.Controls.Add(Me.tbLogo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tbN_Botoes)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbN_Botoes As TextBox
+    Friend WithEvents tbLogo As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents bLerConfig As Button
+    Friend WithEvents bGravarConfig As Button
+End Class
---
+++ Ficheiros/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim ScoreFile As String = "Scores.txt"
+    Dim ConfigFile As String = "Config.txt"
+
+    ' Variaveis globais
+    Dim N_Botoes As Integer = 3
+    Dim Logo As String = "Twin Software"
+
+    Sub LerConfig()
+        If File.Exists(ConfigFile) Then
+            Dim Config As New StreamReader(ConfigFile)
+
+            Do Until (Config.EndOfStream)
+                Dim linha As String = Config.ReadLine
+                If Not linha.StartsWith("#") Then
+                    Dim PosicaoIgual As Integer
+
+                    PosicaoIgual = InStr(1, linha, "=", CompareMethod.Text)
+                    If PosicaoIgual > 0 Then
+                        If linha.Contains("Botoes") Then N_Botoes = Integer.Parse(Mid(linha, PosicaoIgual + 1, 500))
+                        If linha.Contains("Logo") Then Logo = Mid(linha, PosicaoIgual + 1, 500)
+                    End If
+                End If
+            Loop
+
+            Config.Close()
+
+            MsgBox("Configurações lidas com sucesso !!!! ")
+
+        End If
+    End Sub
+
+    Sub GravarConfig()
+        Dim Config As New StreamWriter(ConfigFile, False)
+
+        Config.WriteLine("Botoes=" & tbN_Botoes.Text)
+        Config.WriteLine("Logo=" & tbLogo.Text)
+
+        Config.Close()
+
+        MsgBox("Configurações gravadas com sucesso !!!! ")
+    End Sub
+
+    Sub MostrarConfigs()
+        tbN_Botoes.Text = N_Botoes
+        tbLogo.Text = Logo
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MostrarConfigs()
+    End Sub
+
+    Private Sub bLerConfig_Click(sender As Object, e As EventArgs) Handles bLerConfig.Click
+        LerConfig()
+
+        MostrarConfigs()
+
+    End Sub
+
+    Private Sub bGravarConfig_Click(sender As Object, e As EventArgs) Handles bGravarConfig.Click
+        GravarConfig()
+
+    End Sub
+End Class
